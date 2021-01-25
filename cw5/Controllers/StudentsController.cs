e4f0edb50050efe7c91f5ef67603e4438f390e6f
debskimich <69129514+debskimich@users.noreply.github.com>
2021-01-25 23:18:01 +0100

    cw6

---
+++ cw5/Controllers/StudentsController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using cw5.Models;
+using System.Data.SqlClient;
+
+namespace cw5.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        string ConString = "Data Source=db-mssql;Initial Catalog=s17060;Integrated Security=True";
+        [HttpGet]
+        public IActionResult GetStudents()
+        {
+            var list = new List<Student>();
+
+            using (var con = new SqlConnection(ConString))
+            using (var com = new SqlCommand())
+            {
+                com.Connection = con;
+                com.CommandText = "SELECT * FROM Student";
+
+                con.Open();
+                SqlDataReader dr = com.ExecuteReader();
+                while (dr.Read())
+                {
+                    var st = new Student();
+                    st.FirstName = dr["FirstName"].ToString();
+                    st.LastName = dr["LastName"].ToString();
+                    st.IndexNumber = dr["IndexNumber"].ToString();
+                    list.Add(st);
+                }
+            }
+            return Ok(list);
+        }
+
+        [HttpGet("(id)")]
+        public IActionResult GetStudent(string id)
+        {
+            using (var con = new SqlConnection(ConString))
+            using (var com = new SqlCommand())
+            {
+                com.Connection = con;
+                com.CommandText = "SELECT * FROM Student WHERE IndexNumber=@index";
+
+                com.Parameters.AddWithValue("index", id);
+
+                con.Open();
+                var dr = com.ExecuteReader();
+                if (dr.Read())
+                {
+                    var st = new Student();
+                    st.FirstName = dr["FirstName"].ToString();
+                    st.LastName = dr["LastName"].ToString();
+                    st.IndexNumber = dr["IndexNumber"].ToString();
+                    return Ok(st);
+                }
+            }
+
+            return NotFound();
+        }
+    }
+
+
+}
